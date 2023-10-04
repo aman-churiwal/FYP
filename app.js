16f2c32fb73f4d4b8b9dc69ca44fab636524b511
Amankumar Churiwal <amanchuriwal22@gmail.com>
2023-10-04 14:28:29 +0530

    Auth verification

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import authRoutes from './routes/auth.js'
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/auth', authRoutes)
 
 dotenv.config()
 
